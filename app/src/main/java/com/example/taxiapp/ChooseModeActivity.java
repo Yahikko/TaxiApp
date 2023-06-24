eb6c8c6936d90de2a24f98379814f0399523bfbe
Sendzegakhara <sendzegakhara@mail.ru>
2023-06-24 20:55:55 +0300

    start, choose, passenger, driver layouts; validation

---
+++ app/src/main/java/com/example/taxiapp/ChooseModeActivity.java
@@ -0,0 +1,26 @@
+package com.example.taxiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ChooseModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoose_mode);
+    }
+
+    public void goToPassengerSignIn(View view) {
+        startActivity(new Intent(ChooseModeActivity.this,
+                PassengerSignInActivity.class));
+    }
+
+    public void goToDriverSignIn(View view) {
+        startActivity(new Intent(ChooseModeActivity.this,
+                DriverSignInActivity.class));
+    }
+}
---
+++ app/src/main/java/com/example/taxiapp/DriverSignInActivity.java
@@ -0,0 +1,120 @@
+package com.example.taxiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class DriverSignInActivity extends AppCompatActivity {
+
+    private TextInputLayout textInputEmail;
+    private TextInputLayout textInputName;
+    private TextInputLayout textInputPassword;
+    private TextInputLayout textInputConfirmPassword;
+
+    private Button loginSignUpButton;
+    private TextView toggleLoginSignUpTextView;
+
+    private boolean isLoginModeActive = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_sign_in);
+
+        textInputEmail = findViewById(R.id.textInputEmail);
+        textInputName = findViewById(R.id.textInputName);
+        textInputPassword = findViewById(R.id.textInputPassword);
+        textInputConfirmPassword = findViewById(R.id.textInputConfirmPassword);
+
+        loginSignUpButton = findViewById(R.id.loginSignUpButton);
+        toggleLoginSignUpTextView = findViewById(R.id.toggleLoginSightUpTextView);
+    }
+
+    private boolean validateEmail() {
+
+        String emailInput = textInputEmail.getEditText().getText().toString().trim();
+
+        if (emailInput.isEmpty()) {
+            textInputEmail.setError("Please input your email");
+            return false;
+        } else {
+            textInputEmail.setError("");
+            return true;
+        }
+    }
+
+    private boolean validateName() {
+
+        String nameInput = textInputName.getEditText().getText().toString().trim();
+
+        if (nameInput.isEmpty()) {
+            textInputName.setError("Please input your name");
+            return false;
+        } else if (nameInput.length() > 15) {
+            textInputName.setError("Name length have to be less than 15");
+            return false;
+        } else {
+            textInputName.setError("");
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+
+        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
+        String confirmPasswordInput = textInputConfirmPassword.getEditText()
+                .getText().toString().trim();
+
+        if (passwordInput.isEmpty()) {
+            textInputPassword.setError("Please input your password");
+            return false;
+        } else if (passwordInput.length() < 7) {
+            textInputPassword.setError("Password length have to be more than 6");
+            return false;
+        } else if (!passwordInput.equals(confirmPasswordInput)) {
+            textInputPassword.setError("Passwords have to match");
+            return false;
+        } else {
+            textInputPassword.setError("");
+            return true;
+        }
+    }
+
+    public void loginSignUpUser(View view) {
+
+        if (!validateEmail() | !validateName() | !validatePassword()) {
+            return;
+        }
+
+        String userInput = "Email: " + textInputEmail.getEditText()
+                .getText().toString().trim() + "\nName: " + textInputName.getEditText()
+                .getText().toString().trim() + "\nPassword: " + textInputPassword
+                .getEditText().getText().toString().trim();
+
+        Toast.makeText(this, userInput, Toast.LENGTH_LONG).show();
+    }
+
+    public void toggleLoginSignUp(View view) {
+
+        if (isLoginModeActive) {
+            isLoginModeActive = false;
+            loginSignUpButton.setText("Sign Up");
+            toggleLoginSignUpTextView.setText("Or, log in");
+            textInputConfirmPassword.setVisibility(View.VISIBLE);
+            textInputName.setVisibility(View.VISIBLE);
+        } else {
+            isLoginModeActive = true;
+            loginSignUpButton.setText("Log In");
+            toggleLoginSignUpTextView.setText("Or, sign up");
+            textInputConfirmPassword.setVisibility(View.GONE);
+            textInputName.setVisibility(View.GONE);
+        }
+    }
+}
